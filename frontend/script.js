33cdea05c6d5d97fd001ac47c5f4b39cc491122d
Guilherme-marquesss <carvalhogui.marques@gmail.com>
2025-10-28 17:54:55 -0300

    grok

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 // Produção (site HTTPS): "wss://SEU_BACKEND/?from=site"
-// Dev local:             "ws://localhost:8080/?from=site"
-const ENDERECO_WS = "ws://localhost:8080/?from=site"
+// Dev local:             "ws://localhost:8080/?from=site **"
+const ENDERECO_WS = "wss://unthronged-crinkly-meagan.ngrok-free.dev/?from=site"
 
 const statusConexao = document.getElementById("status")
 const valorTemperatura = document.getElementById("temp")
